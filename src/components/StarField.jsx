c862ceb00b2b0aae450310776047e801427ffd5e
sunminji <tjsalswl1004@naver.com>
2026-04-07 17:20:06 +0900

    파스텔 테마 디자인 + 모바일 최적화

--- src/components/StarField.jsx
+++ src/components/StarField.jsx
@@ -1,14 +1,17 @@
 import { useMemo } from 'react'
 
+const COLORS = ['#c4b5fd', '#f9a8d4', '#fbcfe8', '#ddd6fe', '#fecdd3', '#bfdbfe']
+
 export default function StarField() {
   const stars = useMemo(() => {
-    return Array.from({ length: 80 }, (_, i) => ({
+    return Array.from({ length: 60 }, (_, i) => ({
       id: i,
       x: Math.random() * 100,
       y: Math.random() * 100,
-      size: Math.random() * 2.5 + 0.5,
+      size: Math.random() * 3 + 1,
       delay: Math.random() * 4,
       duration: Math.random() * 3 + 2,
+      color: COLORS[Math.floor(Math.random() * COLORS.length)],
     }))
   }, [])
 
@@ -24,7 +27,8 @@ export default function StarField() {
             width: `${s.size}px`,
             height: `${s.size}px`,
             borderRadius: '50%',
-            background: '#fff',
+            background: s.color,
+            opacity: 0.5,
             animation: `twinkle ${s.duration}s ${s.delay}s infinite ease-in-out`,
           }}
         />
